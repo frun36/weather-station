2bb01a6c4962326769f96b3fe2d40529531604b1
frun36 <franek.urbanski@gmail.com>
2024-11-10 16:44:58 +0100

    Setup WIFI scan example

--- src/main.rs
+++ src/main.rs
@@ -1,165 +1,71 @@
+//! This example uses the RP Pico W board Wifi chip (cyw43).
+//! Scans Wifi for ssid names.
+
 #![no_std]
 #![no_main]
+#![allow(async_fn_in_trait)]
 
-use cortex_m::delay::Delay;
+use core::str;
 
-use bsp::{
-    entry,
-    hal::{
-        clocks::{init_clocks_and_plls, Clock},
-        gpio::{DynPinId, FunctionSio, InOutPin, Pin, PullDown, SioOutput},
-        pac,
-        sio::Sio,
-        watchdog::Watchdog,
-        Timer,
-    },
-};
-use rp_pico as bsp;
+use cyw43_pio::PioSpi;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::pio::{InterruptHandler, Pio};
+use static_cell::StaticCell;
+use {defmt_rtt as _, panic_probe as _};
 
-use defmt_rtt as _;
-use panic_probe as _;
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
 
-use mini_float::f8;
-
-use button::Button;
-use dht11::{DHT11Pin, Dht11, Measurement};
-use display::Display;
-
-type LEDPin = Pin<DynPinId, FunctionSio<SioOutput>, PullDown>;
-
-mod button;
-mod dht11;
-mod display;
-
-enum Command {
-    Measure,
-    Fun,
+#[embassy_executor::task]
+async fn cyw43_task(runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>) -> ! {
+    runner.run().await
 }
 
-#[entry]
-fn main() -> ! {
-    // Initialize basic peripherals
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
+#[embassy_executor::task]
+async fn net_task(mut runner: embassy_net::Runner<'static, cyw43::NetDriver<'static>>) -> ! {
+    runner.run().await
+}
 
-    let pins = bsp::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    info!("Hello World!");
 
-    // Initialize clocks, delay and timer
-    let clocks = init_clocks_and_plls(
-        bsp::XOSC_CRYSTAL_FREQ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
+    let p = embassy_rp::init(Default::default());
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
+    let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
 
-    let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+    // To make flashing faster for development, you may want to flash the firmwares independently
+    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
+    //     probe-rs download 43439A0.bin --binary-format bin --chip RP2040 --base-address 0x10100000
+    //     probe-rs download 43439A0_clm.bin --binary-format bin --chip RP2040 --base-address 0x10140000
+    //let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
+    //let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
 
-    // Initialize devices
-    let mut display = Display::new(
-        pins.gpio2.into_push_pull_output().into_dyn_pin(),
-        pins.gpio3.into_push_pull_output().into_dyn_pin(),
-        pins.gpio4.into_push_pull_output().into_dyn_pin(),
-        pins.gpio5.into_push_pull_output().into_dyn_pin(),
-        pins.gpio6.into_push_pull_output().into_dyn_pin(),
-        pins.gpio7.into_push_pull_output().into_dyn_pin(),
-        pins.gpio8.into_push_pull_output().into_dyn_pin(),
-        pins.gpio9.into_push_pull_output().into_dyn_pin(),
-    );
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(&mut pio.common, pio.sm0, pio.irq0, cs, p.PIN_24, p.PIN_29, p.DMA_CH0);
 
-    let button = Button::new(pins.gpio15.into_pull_down_input().into_dyn_pin());
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(cyw43_task(runner)));
 
-    let dht11_pin: DHT11Pin = InOutPin::new(pins.gpio16);
-    let mut dht11 = Dht11::new(dht11_pin);
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
 
-    // Blink display
-    display.enable_all();
-    delay.delay_ms(1_000);
-    display.disable_all();
-
-    // Main loop
-    let mut command;
-    loop {
-        if button.is_clicked().unwrap() {
-            command = Command::Measure;
-            if button.await_next_click(&mut delay, &timer).unwrap() {
-                command = Command::Fun;
-            }
-            match command {
-                Command::Measure => {
-                    measurement_cycle(&mut dht11, &mut display, &mut delay);
-                    // display.roll_fwd(&mut delay, 100);
-                    // display.disable_all();
-                }
-                Command::Fun => {
-                    display.roll_bwd(&mut delay, 100);
-                    display.disable_all();
-                }
-            }
+    let mut scanner = control.scan(Default::default()).await;
+    while let Some(bss) = scanner.next().await {
+        if let Ok(ssid_str) = str::from_utf8(&bss.ssid) {
+            info!("scanned {} == {:x}", ssid_str, bss.bssid);
         }
-    }
-}
-
-/// The whole process of measurement and showing the results
-fn measurement_cycle(dht11: &mut Dht11, display: &mut Display, delay: &mut Delay) {
-    display.roll_fwd(delay, 128);
-    delay.delay_ms(256);
-    display.disable_all();
-
-    let measurement = dht11.perform_measurement(delay).unwrap_or_else(|e| {
-        match_error(e, display, delay);
-        Measurement {
-            temperature: 0,
-            humidity: 0,
-        }
-    });
-
-    if measurement.temperature == 0 && measurement.humidity == 0 {
-        return;
-    }
-
-    let temperature = measurement.temperature as f32 * 0.1_f32; // Convert to Celsius
-    let humidity = measurement.humidity as f32 * 0.001_f32; // Convert to fraction
-
-    display.display_f8(f8::from_f32(temperature));
-    delay.delay_ms(10_000);
-    display.disable_all();
-
-    delay.delay_ms(200);
-
-    display.display_f8(f8::from_f32(humidity));
-    delay.delay_ms(10_000);
-    display.disable_all();
-
-    display.roll_bwd(delay, 128);
-    delay.delay_ms(256);
-    display.disable_all();
-}
-
-/// Displays correct error code
-fn match_error(e: dht11::Error, display: &mut Display, delay: &mut Delay) {
-    let error_code: u8 = match e {
-        dht11::Error::Timeout => 0x81,
-        dht11::Error::CrcMismatch => 0x82,
-        dht11::Error::Gpio(_) => 0x84,
-    };
-    for _ in 0..3 {
-        display.display_u8(error_code);
-        delay.delay_ms(200);
-        display.disable_all();
-        delay.delay_ms(200);
     }
 }
